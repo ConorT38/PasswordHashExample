f5f99d43c4baa5e53c8405eb1f8c7eaa10e07519
Conor Thompson <conorthompson213@gmail.com>
2015-09-21 13:04:06 +0100

    Create PwdHashExamp.php

---
+++ PwdHashExamp.php
@@ -0,0 +1,20 @@
+$pass= $_POST['pass'];
+  
+$pass = hash('sha512', $pass);
+$pass = hash('sha512', $pass);
+
+$pass = md5($pass);
+$pass = md5($pass);
+
+$pass = hash('sha256', $pass);
+$pass = hash('sha256', $pass);
+
+$pass = md5($pass);
+$pass = md5($pass);
+
+$pass = hash('sha512', $pass);
+$pass = hash('sha512', $pass);
+
+
+
+
