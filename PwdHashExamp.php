dc4e91fc22ff1dba852131c29e163764ad48caa4
Conor Thompson <conorthompson213@gmail.com>
2016-06-21 09:39:08 -0400

    Update PwdHashExamp.php

--- PwdHashExamp.php
+++ PwdHashExamp.php
@@ -1,22 +1,30 @@
 <?php
-$pass= $_POST['pass'];
-  
-$pass = hash('sha512', $pass);
-$pass = hash('sha512', $pass);
+define("PBKDF2_HASH_ALGORITHM", "sha256");
+define("PBKDF2_ITERATIONS", 1000);
+define("PBKDF2_SALT_BYTE_SIZE", 24);
+define("PBKDF2_HASH_BYTE_SIZE", 24);
 
-$pass = md5($pass);
-$pass = md5($pass);
+define("HASH_SECTIONS", 4);
+define("HASH_ALGORITHM_INDEX", 0);
+define("HASH_ITERATION_INDEX", 1);
+define("HASH_SALT_INDEX", 2);
+define("HASH_PBKDF2_INDEX", 3);
 
-$pass = hash('sha256', $pass);
-$pass = hash('sha256', $pass);
-
-$pass = md5($pass);
-$pass = md5($pass);
-
-$pass = hash('sha512', $pass);
-$pass = hash('sha512', $pass);
-
-echo $pass;
+/** Hashes can not be relied on being the same twice */
+function create_hash($password)
+{
+    // format: algorithm:iterations:salt:hash
+    $salt = base64_encode(mcrypt_create_iv(PBKDF2_SALT_BYTE_SIZE, MCRYPT_DEV_URANDOM));
+    return "derp".PBKDF2_HASH_ALGORITHM . ":" . PBKDF2_ITERATIONS . ":" .  $salt . ":" .
+        base64_encode(pbkdf2(
+            PBKDF2_HASH_ALGORITHM,
+            $password,
+            $salt,
+            PBKDF2_ITERATIONS,
+            PBKDF2_HASH_BYTE_SIZE,
+            true
+        ));
+}
 ?>
 
 
